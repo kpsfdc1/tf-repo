d78e4c72288dd6ec55f9ae0cb8d3c8158f74bdc2
root <root@ip-172-31-43-58.ap-south-1.compute.internal>
2023-08-06 04:45:42 +0000

    user1 main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws"{
+	region = "ap-south-1"
+}
+
+terraform{
+	backend "s3" {
+		bucket = "tf-new-bucket"
+		key = "terraform.tfstate"
+		region = "ap-south-1"
+	}
+}
+
+resource "aws_instance" "inst" {
+	ami= "ami-08e5424edfe926b43"
+	instance_type = "t2.micro"
+	tags = {
+		Name = "dev-inst"
+	}
+}
